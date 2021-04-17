11d33b84444fdaa339591ba330de2832dd7c08db
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-17 11:01:45 +0300

    DONE

--- lab_03/main.py
+++ lab_03/main.py
@@ -1,8 +1,4 @@
-import numpy as np
-from itertools import product
-
 from settings import PATH_TO_TABLE
-from polynomes.newton_polyn import bilinear_interp, newton_polyn
 from polynomes.utils import read_table
 from polynomes.splines import spline_interpolation
 
--- lab_03/polynomes/splines.py
+++ lab_03/polynomes/splines.py
@@ -1,25 +1,23 @@
-from typing import Tuple
 import numpy as np
-from bisect import bisect
-from dataclasses import dataclass
 
-from numpy.lib.polynomial import poly
-from polynomes.utils import parse_table
+from typing import Tuple
+from bisect import bisect
 
 
 def _get_h(table: np.ndarray, index: int) -> float:
     return table[index, 0] - table[index-1, 0]
 
-def _get_c_coef(table: np.ndarray, index: int):
+
+def _get_c_coef(table: np.ndarray, index: int) -> float:
     def _get_f(i: int) -> float:
         ydelta = lambda i: table[i, 1] - table[i-1, 1]
         return 3 * (ydelta(i) / _get_h(table, i) - ydelta(i-1) / _get_h(table, i-1))
 
-    def _get_eta(eta_prev: float, xi_prev: float, i: int):
+    def _get_eta(eta_prev: float, xi_prev: float, i: int) -> float:
         hr, hl = _get_h(table, i), _get_h(table, i-1)
         return (_get_f(i) - hl * eta_prev) / (hl * xi_prev + 2 * (hl + hr))
 
-    def _get_xi(xi_prev: float, i: int):
+    def _get_xi(xi_prev: float, i: int) -> float:
         hr, hl = _get_h(table, i), _get_h(table, i-1)
         return -hr / (hl * xi_prev + 2 * (hl + hr))
 
@@ -39,22 +37,27 @@ def _get_c_coef(table: np.ndarray, index: int):
         
     return cl, cr
 
-def _get_a_coef(table: np.ndarray, index) -> None:
+
+def _get_a_coef(table: np.ndarray, index: int) -> float:
     return table[index-1, 1]
 
-def _get_b_coef(table: np.ndarray, cl, cr, index, hi) -> None:
+
+def _get_b_coef(table: np.ndarray, cl: float, cr: float, index: int, hi: float) -> float:
     ydelta = lambda i: table[i, 1] - table[i-1, 1]
     return (ydelta(index)) / hi - hi * (cr + 2 * cl) / 3
 
-def _get_d_coef(cl, cr, hi) -> float:
+
+def _get_d_coef(cl: float, cr: float, hi: float) -> float:
     return (cr - cl) / (3 * hi)
 
-def _get_coeffs(table: np.ndarray, index, hi) -> Tuple[float, float, float, float, float]:
+
+def _get_coeffs(table: np.ndarray, index: int, hi: float) -> Tuple[float, float, float, float, float]:
     cl, cr = _get_c_coef(table, index)
     a = _get_a_coef(table, index)
     b = _get_b_coef(table, cl, cr, index, hi)
     d = _get_d_coef(cl, cr, hi)
     return a, b, cl, d
+
 
 def spline_interpolation(x: float, table: np.ndarray) -> float:
     index = bisect(table[:, 0], x)
