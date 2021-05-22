792e96e7e1daf16092bcb78d746d1cd883a4de4a
d_ivahnencko <ivahnencko01@gmail.com>
2021-05-22 05:02:20 +0300

    add listing

--- lab_06/code/main.py
+++ lab_06/code/main.py
@@ -2,21 +2,21 @@ from .solve import Calculator
 
 
 def main():
-    x = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-    y = [0.571, 0.889, 1.091, 1.231, 1.333, 1.412]
+    x_values = (1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
+    y_values = (0.571, 0.889, 1.091, 1.231, 1.333, 1.412)
     h = 1.0
 
-    Calculator.print_init("X              :", x)
-    Calculator.print_init("Y              :", y)
-    Calculator.print_res("Onesided       :", Calculator.left(y, h))
-    Calculator.print_res("Center         :",
-                         Calculator.center(y, h))
-    Calculator.print_res("Second Runge   :",
-                         Calculator.second_runge(y, h, 1))
-    Calculator.print_res("Aligned params :",
-                         Calculator.aligned_vars(x, y))
-    Calculator.print_res("Second onesided:",
-                         Calculator.second_left(y, h))
+    Calculator.print_init("X              :", x_values)
+    Calculator.print_init("Y              :", y_values)
+    Calculator.print_res("1. Односторонняя разностная: ",
+                         Calculator.left(y_values, h))
+    Calculator.print_res("2. Центральная: ", Calculator.center(y_values, h))
+    Calculator.print_res("3. 2-я формала Рунге: ",
+                         Calculator.second_runge(y_values, h, 1))
+    Calculator.print_res("4. Выравнивающие переменные: ",
+                         Calculator.aligned_vars(x_values, y_values))
+    Calculator.print_res("5. Вторая разностная:",
+                         Calculator.second_left(y_values, h))
 
 
 if __name__ == "__main__":
